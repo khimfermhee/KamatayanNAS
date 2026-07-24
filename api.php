cc81f1aed88dfd13ad6248648f028d43196fb772
khimfermheeronquillo@gmail.com <khimfermheeronquillo@gmail.com>
2026-07-24 17:06:35 +0800

    Fix: Enhance video streaming speed with HTTP Range + Add custom logo

--- api.php
+++ api.php
@@ -224,10 +224,56 @@ switch ($action) {
         ];
         
         $mime = $mime_types[$ext] ?? 'application/octet-stream';
+        $size = filesize($fullPath);
+        $time = date('r', filemtime($fullPath));
         
-        header('Content-Type: ' . $mime);
-        header('Content-Length: ' . filesize($fullPath));
-        readfile($fullPath);
+        $fm = @fopen($fullPath, 'rb');
+        if (!$fm) {
+            http_response_code(500);
+            die("Could not open file");
+        }
+
+        $begin = 0;
+        $end = $size - 1;
+
+        if (isset($_SERVER['HTTP_RANGE'])) {
+            if (preg_match('/bytes=\h*(\d+)-(\d*)[\D.*]?/i', $_SERVER['HTTP_RANGE'], $matches)) {
+                $begin = intval($matches[1]);
+                if (!empty($matches[2])) {
+                    $end = intval($matches[2]);
+                }
+            }
+        }
+
+        if (isset($_SERVER['HTTP_RANGE'])) {
+            header('HTTP/1.1 206 Partial Content');
+        } else {
+            header('HTTP/1.1 200 OK');
+        }
+
+        header("Content-Type: $mime");
+        header('Cache-Control: public, must-revalidate, max-age=0');
+        header('Pragma: no-cache');  
+        header('Accept-Ranges: bytes');
+        header('Content-Length:' . (($end - $begin) + 1));
+        if (isset($_SERVER['HTTP_RANGE'])) {
+            header("Content-Range: bytes $begin-$end/$size");
+        }
+        header("Content-Disposition: inline; filename=".basename($fullPath));
+        header("Content-Transfer-Encoding: binary");
+        header("Last-Modified: $time");
+
+        $cur = $begin;
+        fseek($fm, $begin, 0);
+
+        // Stream file in chunks (16KB)
+        while(!feof($fm) && $cur <= $end && (connection_status() == 0)) {
+            print fread($fm, min(1024 * 16, ($end - $cur) + 1));
+            $cur += 1024 * 16;
+            flush();
+        }
+        
+        fclose($fm);
         exit;
 
     case 'thumbnail':
